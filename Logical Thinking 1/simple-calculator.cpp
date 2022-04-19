71797b9ca9b198e837a3a726150595ae9e87d00f
Chinmay Vivek <chinmay.vivek@gmail.com>
2022-04-19 20:42:09 +0530

    [INIT]:  A Simple Calculator

---
+++ Logical Thinking 1/simple-calculator.cpp
@@ -0,0 +1,50 @@
+//  A Simple Calculator
+
+//Problem Description
+// Given two numbers A and B. Print A+B, A*B, A-B, A/B in the same order.
+
+// Problem Constraints
+// 1 <= A, B <= 10^4
+
+// Input Format
+// There are two input lines
+// The first line has a single integer A.
+// The second line has a single integer B.
+
+// Output Format
+// Print in a single line separated by space A+B, A*B, A-B, A/B in the same order.
+
+// Example Input
+// Input 1:-
+
+// 4
+// 5
+
+// Input 2:-
+
+// 16
+// 2
+
+// Example Output
+// Output 1:-
+
+// 9 20 -1 0
+
+// Output 2:-
+
+// 18 32 14 8
+
+
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+   
+    int A =70 ;
+    int B = 24;
+
+    cout <<A+B <<" "<<A*B <<" "<<A-B << " "<< A/B;
+    
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # scaling-spoon
 This repository conatins basic to advance programming Practice
+## CPP
