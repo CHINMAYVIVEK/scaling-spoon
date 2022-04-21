fab0dfc9dc691b26f54243fdab4a874c59ab2083
Chinmay Vivek <chinmay.vivek@gmail.com>
2022-04-22 02:41:25 +0530

    [UPDT]: Practice Question updated

---
+++ More-Practice/CheckEvenOdd.java
@@ -0,0 +1,69 @@
+// Check even/odd
+
+/**
+ * 
+ * Problem Description
+
+Write a program to input an integer from user and print 1 if it is odd otherwise print 0.
+
+Problem Constraints
+1 <= N <= 1000000
+
+Input Format
+One line containing an integer N.
+
+Output Format
+Print either 1 or 0 as per the question.
+
+Example Input
+
+Input 1:
+
+5
+
+Input 2:
+
+1000
+
+Example Output
+
+Output 1:
+
+1
+
+Output 2:
+
+0
+
+Example Explanation
+
+Explanation 1:
+
+Clearly, 5 is odd.
+
+Explanation 2:
+
+Clearly, 1000 is even.
+
+ */
+
+import java.lang.*;
+import java.util.*;
+
+public class CheckEvenOdd {
+    public static void main(String[] args) {
+        
+        try (Scanner sc = new Scanner(System.in)){
+
+            int A = sc.nextInt();
+
+            if(A%2==0){
+                System.out.println("0");
+            }
+            else{
+                System.out.println("1");
+            }
+
+        }
+    }
+}
